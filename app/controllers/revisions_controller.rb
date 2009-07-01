472c9f1ba44e36dca5e1d55ce3e6a3e363f5fd0c
Julian Burgess <julian@opening-times.co.uk>
2009-07-01 09:20:38 +0100

    Render revision show action as XML

--- app/controllers/revisions_controller.rb
+++ app/controllers/revisions_controller.rb
@@ -1,6 +1,7 @@
 class RevisionsController < ApplicationController
 
   def index
+    @revision_limit = 100
     @facility = Facility.find(params[:facility_id])
     @revisions = @facility.facility_revisions.find(:all, :order => 'id DESC', :limit => 100)
   end
@@ -8,12 +9,7 @@ class RevisionsController < ApplicationController
   def show
     @facility = Facility.find(params[:facility_id])
     @revision = @facility.facility_revisions.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => @facility }
-      format.json  { render :json => @facility }
-    end
+    render :xml => @revision.xml
   end
 
 end
