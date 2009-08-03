33d5d69ddb886fae0de94861ae22863e4bc8b25c
Julian Burgess <julian@opening-times.co.uk>
2009-08-03 18:00:59 +0100

    Changed register to sign up and tidied

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.login "login", :controller => "user_sessions", :action => "new"
   map.logout "logout", :controller => "user_sessions", :action => "destroy"
+  map.signup "signup", :controller => "users", :action => "new"
 
   map.resources :user_sessions
   map.resources :users
