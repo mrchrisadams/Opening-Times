43e4235f702e46f12c8e7250532f46f66b114c20
Julian Burgess <julian@opening-times.co.uk>
2009-08-26 23:02:47 +0100

    More reports, almost becoming useful

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,9 @@
 class ReportsController < ApplicationController
 
+  DEFAULT_LIMIT = 100
+  MAX_LIMIT = 1000
+
+  before_filter :set_limit
 
   def long_locations
     @facilities = Facility.paginate(:all, :order => 'LENGTH(location) DESC', :page => params[:page])
@@ -10,23 +14,25 @@ class ReportsController < ApplicationController
   end
 
   def openings_count
-    @limit = (params[:limit] || 100).to_i
-    @limit = 100 if @limit > 1000
     @min = (params[:min] || 7).to_i
     @max = (params[:max] || 7).to_i
     @max = @min if @min > @max
     @facilities = Facility.find_by_sql("SELECT facilities.id, name, location, updated_at, count(openings.id) AS openings_count FROM facilities LEFT JOIN openings ON facilities.id = facility_id GROUP BY facilities.id HAVING openings_count < #{@min} or openings_count > #{@max} ORDER BY updated_at DESC LIMIT #{@limit}")
   end
 
+  def openings_length
+    @min = params[:min].to_i
+    @max = params[:max].to_i
+    @max = @min if @min > @max
+    @openings = Opening.paginate(:all, :include => :facility, :conditions => ["(closes_mins-opens_mins) BETWEEN ? AND ?",@min, @max], :order => 'id DESC', :page => params[:page])
+  end
+
+
+
 #  def between_openings
 #    @min_length = params[:min] || 5
 #    @max_length = params[:max] || 24
 #    @openings = Opening.find(:all, :conditions=>"IF(closes_mins=0,1440,closes_mins)-opens_mins < #{@min_length} or IF(closes_mins=0,1440,closes_mins)-opens_mins < #{@max_length}", :order => "IF(closes_mins=0,1440,closes_mins)-opens_mins")
-#  end
-
-#  def length_openings
-#    @length = params[:length]    
-#    @openings = Opening.find(:all,:include=>:service,:conditions=>["(IF(closes_mins=0,1440,closes_mins)-opens_mins)=?",@length],:order=>'services.name,sequence,opens_mins')
 #  end
 
 #  
@@ -35,5 +41,12 @@ class ReportsController < ApplicationController
 
 #  end
 
+
+  private
+  
+  def set_limit
+    @limit = params[:limit].to_i
+    @limit = DEFAULT_LIMIT if @limit > MAX_LIMIT || @limit == 0
+  end
 
 end
