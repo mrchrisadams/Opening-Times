420f39ad8fcf545a36a9e9daf57305219a713560
Julian Burgess <julian@opening-times.co.uk>
2009-08-26 23:27:01 +0100

    Find by postcode (from postal district)

--- app/models/holiday_set.rb
+++ app/models/holiday_set.rb
@@ -7,5 +7,17 @@ class HolidaySet < ActiveRecord::Base
 
   validates_length_of :name, :maximum => 100
 
+  def self.find_by_postcode(postcode)
+    case 
+      when postcode =~ /^TD|DG|KA|ML|EH|KY|FK|G|PA|PH|AB|IV|KW/
+        return find_by_name("Scotland")
+      when postcode =~ /^BT/
+        return find_by_name("Northern Ireland")
+      else
+        return find_by_name("England & Wales")
+    end
+  end
+
+
 end
 
