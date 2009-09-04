c588f5cfae5e8861124e749e28b3bd6cf9a74ff0
Julian Burgess <julian@opening-times.co.uk>
2009-09-04 21:48:07 +0100

    Added newrelic support

--- config/environment.rb
+++ config/environment.rb
@@ -18,12 +18,12 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "hpricot"
   config.gem "authlogic"
-  config.gem "geokit"
   config.gem "fireeagle"
+  config.gem "geokit"
+  config.gem "hpricot"
   config.gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate', :source => 'http://gems.github.com'
-
+  config.gem "newrelic_rpm"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
